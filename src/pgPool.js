b5803595e69239704305a803eb91822ee0957e4e
jean-baptiste cazaux <jbcazaux@gmail.com>
2021-03-02 11:17:55 +0100

    logs connections in db (#2)

---
+++ src/pgPool.js
@@ -0,0 +1,5 @@
+import pg from 'pg'
+
+const pgPool = new pg.Pool()
+
+export default pgPool
--- src/server.js
+++ src/server.js
@@ -1,17 +1,42 @@
-'use strict'
 import Koa from 'koa'
 import hello from './hello.js'
+import pgPool from './pgPool.js'
+import Router from '@koa/router'
 
 const app = new Koa()
+const router = new Router()
 const PORT = 80
 const HOST = '0.0.0.0'
 
 const port = process.env.PORT || PORT
 
-app.use(async ctx => {
+router.get('/', async ctx => {
+  let client
+  try {
+    client = await pgPool.connect()
+    const clientIP = ctx.request.ip
+    await client.query('INSERT INTO visitors (ip, visit_date) VALUES ($1, $2)', [clientIP, new Date()])
+  } finally {
+    client?.release()
+  }
   ctx.body = hello('world')
 })
 
-app.listen(port)
+router.get('/visitors', async ctx => {
+  let client
+  try {
+    client = await pgPool.connect()
+    const result = await client.query('SELECT ip, visit_date FROM visitors')
+    ctx.body = result.rows.map(r => ({ [r.ip]: r.visit_date }))
+  } finally {
+    client?.release()
+  }
+})
+
+router.post('/', ctx => {
+  ctx.body = 'post !'
+})
+
+app.use(router.routes()).use(router.allowedMethods()).listen(port)
 
 console.log(`Running on http://${HOST}:${PORT}`)
